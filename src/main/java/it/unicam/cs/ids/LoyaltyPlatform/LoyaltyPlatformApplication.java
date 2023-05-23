54143718c6e87a45238124ddc44b82acb23bc6b5
loris minetti <lorisminetti1997@gmail.com>
2023-05-23 16:49:45 +0200

    datasource exclude

--- src/main/java/it/unicam/cs/ids/LoyaltyPlatform/LoyaltyPlatformApplication.java
+++ src/main/java/it/unicam/cs/ids/LoyaltyPlatform/LoyaltyPlatformApplication.java
@@ -2,8 +2,9 @@ package it.unicam.cs.ids.LoyaltyPlatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }) // Disabilita la connessione al database
 public class LoyaltyPlatformApplication {
 
 	public static void main(String[] args) {
