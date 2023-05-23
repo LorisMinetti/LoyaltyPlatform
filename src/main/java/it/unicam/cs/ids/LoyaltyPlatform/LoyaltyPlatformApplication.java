ae852ca1a12de9c038f28bede0ffdaff72b5d786
loris minetti <lorisminetti1997@gmail.com>
2023-05-23 16:12:05 +0200

    first creational commit

---
+++ src/main/java/it/unicam/cs/ids/LoyaltyPlatform/LoyaltyPlatformApplication.java
@@ -0,0 +1,12 @@
+package it.unicam.cs.ids.LoyaltyPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyPlatformApplication.class, args);	}
+
+}
---
+++ src/test/java/it/unicam/cs/ids/LoyaltyPlatform/LoyaltyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids.LoyaltyPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
