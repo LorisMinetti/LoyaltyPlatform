11c472aedd063d9c69d909ccde1ee79a34ad3eb4
loris minetti <lorisminetti1997@gmail.com>
2023-05-25 15:46:23 +0200

    first 'inbound' repository

---
+++ src/main/java/it/unicam/cs/ids/LoyaltyPlatform/repository/inbound/ClienteRepository.java
@@ -0,0 +1,16 @@
+package it.unicam.cs.ids.LoyaltyPlatform.repository.inbound;
+
+import it.unicam.cs.ids.LoyaltyPlatform.model.ClienteModel;
+
+import java.util.UUID;
+
+public interface ClienteRepository {
+    void save(ClienteModel clienteModel);
+
+    void update(ClienteModel clienteModel);
+
+    boolean delete(ClienteModel clienteModel);
+
+    ClienteModel findById(UUID id);
+
+}
