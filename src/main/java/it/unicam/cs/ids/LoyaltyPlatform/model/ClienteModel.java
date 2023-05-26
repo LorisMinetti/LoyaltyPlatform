ec98d6a28bbcddb2f9c6e384e9e1f1c2417a21e1
loris minetti <lorisminetti1997@gmail.com>
2023-05-26 16:59:45 +0200

    Builder pattern implemented

--- src/main/java/it/unicam/cs/ids/LoyaltyPlatform/model/ClienteModel.java
+++ src/main/java/it/unicam/cs/ids/LoyaltyPlatform/model/ClienteModel.java
@@ -19,4 +19,54 @@ public class ClienteModel {
     private Map<ProgrammaAPuntiModel, Integer> puntiPerAttivitaCommerciale;
     private Map<ProgrammaCashbackModel, Double> saldoPerAttivitaCommerciale;
 
+
+    /*
+    Builder Pattern
+     */
+    public static class Builder{
+        private String nome;
+        private UUID id;
+        private Map<AttivitaCommercialeController, Double> spesaTotalePerAttivitaCommerciale;
+        private Map<ProgrammaALivelliModel, Integer> livelloPerAttivitaCommerciale;
+        private Map<ProgrammaAPuntiModel, Integer> puntiPerAttivitaCommerciale;
+        private Map<ProgrammaCashbackModel, Double> saldoPerAttivitaCommerciale;
+
+
+        //Metodi setter per i builder
+
+        public Builder nome(String nome){
+            this.nome = nome;
+            return this;
+        }
+        public Builder spesaTotalePerAttivitaCommerciale(Map<AttivitaCommercialeController, Double> spesaTotalePerAttivitaCommerciale){
+            this.spesaTotalePerAttivitaCommerciale = spesaTotalePerAttivitaCommerciale;
+            return this;
+        }
+        public Builder livelloPerAttivitaCommerciale(Map<ProgrammaALivelliModel, Integer> livelloPerAttivitaCommerciale){
+            this.livelloPerAttivitaCommerciale = livelloPerAttivitaCommerciale;
+            return this;
+        }
+        public Builder puntiPerAttivitaCommerciale(Map<ProgrammaAPuntiModel, Integer> puntiPerAttivitaCommerciale){
+            this.puntiPerAttivitaCommerciale = puntiPerAttivitaCommerciale;
+            return this;
+        }
+        public Builder saldoPerAttivitaCommerciale(Map<ProgrammaCashbackModel, Double> saldoPerAttivitaCommerciale){
+            this.saldoPerAttivitaCommerciale = saldoPerAttivitaCommerciale;
+            return this;
+        }
+
+        //Metodo build per il builder
+        public ClienteModel build(){
+            ClienteModel clienteModel = new ClienteModel();
+            clienteModel.nome = this.nome;
+            clienteModel.spesaTotalePerAttivitaCommerciale = this.spesaTotalePerAttivitaCommerciale;
+            clienteModel.livelloPerAttivitaCommerciale = this.livelloPerAttivitaCommerciale;
+            clienteModel.puntiPerAttivitaCommerciale = this.puntiPerAttivitaCommerciale;
+            clienteModel.saldoPerAttivitaCommerciale = this.saldoPerAttivitaCommerciale;
+            return clienteModel;
+        }
+
+
+    }
+
 }
