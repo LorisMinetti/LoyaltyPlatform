538ea0f5abd390a731917dfe8625d9b9986ddb94
tasso01 <francescotasso16@gmail.com>
2023-05-31 12:01:13 +0200

    created ProgrammaCoalizioneRepository

---
+++ src/main/java/it/unicam/cs/ids/LoyaltyPlatform/repository/ProgrammaCoalizioneRepositoryImpl.java
@@ -0,0 +1,80 @@
+package it.unicam.cs.ids.LoyaltyPlatform.repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.unicam.cs.ids.LoyaltyPlatform.model.ProgrammaCoalizioneModel;
+import it.unicam.cs.ids.LoyaltyPlatform.repository.inbound.ProgrammaCoalizioneRepository;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+public class ProgrammaCoalizioneRepositoryImpl implements ProgrammaCoalizioneRepository {
+
+    private static final String OUTPUT_DIR = "C:\\Users\\loris\\Desktop\\LoyaltyPlatform\\programmaCoalizone";
+
+    private final ObjectMapper mapper;
+
+    public ProgrammaCoalizioneRepositoryImpl() { this.mapper = new ObjectMapper(); }
+
+    /*
+     *   Singleton constructor
+     */
+    private static class SingletonBuilder{
+        private static final ProgrammaCoalizioneRepositoryImpl INSTANCE = new ProgrammaCoalizioneRepositoryImpl();
+    }
+    public static ProgrammaCoalizioneRepositoryImpl getInstance(){
+        return ProgrammaCoalizioneRepositoryImpl.SingletonBuilder.INSTANCE;
+    }
+
+    @Override
+    public ProgrammaCoalizioneModel save(ProgrammaCoalizioneModel programmaCoalizioneModel) {
+        try{
+            String fileName = programmaCoalizioneModel.getNome() + ".json";  //usa il nome del programma come nome del file
+            String filePath = OUTPUT_DIR + "\\" + fileName;
+
+            String json = mapper.writeValueAsString(programmaCoalizioneModel);
+
+            Files.write((Paths.get(filePath)), json.getBytes());
+
+
+        } catch (IOException e) {
+            System.err.println("Errore durante il salvataggio del Programma Coalizione: " + e.getMessage());
+        }
+
+        return programmaCoalizioneModel;
+    }
+
+    //TODO
+    @Override
+    public ProgrammaCoalizioneModel update(ProgrammaCoalizioneModel programmaCoalizioneModel) throws IOException {
+        return null;
+    }
+
+    @Override
+    public boolean delete(ProgrammaCoalizioneModel programmaCoalizioneModel) {
+        try {
+            String fileName = programmaCoalizioneModel.getNome() + ".json";  // Usa il nome del programma come nome del file
+            String filePath = OUTPUT_DIR + "\\" + fileName;
+
+            File file = new File(filePath);
+            if (file.exists()) {
+                file.delete();
+                System.out.println("Programma Coalizione eliminato con successo: " + programmaCoalizioneModel);
+                return true;
+            } else {
+                System.out.println("Il file non esiste: " + filePath);
+            }
+        } catch (Exception e) {
+            System.err.println("Errore durante l'eliminazione del Programma: " + e.getMessage());
+        }
+        return false;
+    }
+
+    //TODO
+    @Override
+    public ProgrammaCoalizioneModel findById(UUID id) {
+        return null;
+    }
+}
---
+++ src/main/java/it/unicam/cs/ids/LoyaltyPlatform/repository/inbound/ProgrammaCoalizioneRepository.java
@@ -0,0 +1,18 @@
+package it.unicam.cs.ids.LoyaltyPlatform.repository.inbound;
+
+import it.unicam.cs.ids.LoyaltyPlatform.model.ProgrammaCoalizioneModel;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public interface ProgrammaCoalizioneRepository {
+
+    ProgrammaCoalizioneModel save(ProgrammaCoalizioneModel programmaCoalizioneModel);
+
+    ProgrammaCoalizioneModel update(ProgrammaCoalizioneModel programmaCoalizioneModel) throws IOException;
+
+    boolean delete(ProgrammaCoalizioneModel programmaCoalizioneModel);
+
+    ProgrammaCoalizioneModel findById(UUID id);
+
+}
