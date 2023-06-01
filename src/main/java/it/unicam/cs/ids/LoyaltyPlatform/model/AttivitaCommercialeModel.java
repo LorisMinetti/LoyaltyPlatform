09c29c1a03d82af3313df647e8fe41692eb2f23f
loris minetti <lorisminetti1997@gmail.com>
2023-06-01 17:41:04 +0200

    @Builder @AllArgsConstructor added

--- src/main/java/it/unicam/cs/ids/LoyaltyPlatform/model/AttivitaCommercialeModel.java
+++ src/main/java/it/unicam/cs/ids/LoyaltyPlatform/model/AttivitaCommercialeModel.java
@@ -1,19 +1,21 @@
 package it.unicam.cs.ids.LoyaltyPlatform.model;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import it.unicam.cs.ids.LoyaltyPlatform.controller.inbound.AttivitaCommercialeController;
+import lombok.*;
 
 import java.util.List;
 import java.util.UUID;
 
 @Data
 @EqualsAndHashCode
+@AllArgsConstructor
+@Builder
 @ToString
 public class AttivitaCommercialeModel {
     private String nome;
     private UUID id;
     private String partitaIVA;
     private List<ProgrammaFedeltaModel> programmiFedeltaAderiti;
+
 
 }
--- src/main/java/it/unicam/cs/ids/LoyaltyPlatform/model/ClienteModel.java
+++ src/main/java/it/unicam/cs/ids/LoyaltyPlatform/model/ClienteModel.java
@@ -1,15 +1,14 @@
 package it.unicam.cs.ids.LoyaltyPlatform.model;
 
-import it.unicam.cs.ids.LoyaltyPlatform.controller.inbound.AttivitaCommercialeController;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import lombok.*;
 
 import java.util.Map;
 import java.util.UUID;
 
 @Data
 @EqualsAndHashCode
+@AllArgsConstructor
+@Builder
 @ToString
 public class ClienteModel {
     private String nome;
@@ -20,53 +19,5 @@ public class ClienteModel {
     private Map<ProgrammaCashbackModel, Double> saldoPerAttivitaCommerciale;
 
 
-    /*
-    Builder Pattern
-     */
-    public static class Builder{
-        private String nome;
-        private UUID id;
-        private Map<AttivitaCommercialeModel, Double> spesaTotalePerAttivitaCommerciale;
-        private Map<ProgrammaALivelliModel, Integer> livelloPerAttivitaCommerciale;
-        private Map<ProgrammaAPuntiModel, Integer> puntiPerAttivitaCommerciale;
-        private Map<ProgrammaCashbackModel, Double> saldoPerAttivitaCommerciale;
-
-
-        //Metodi setter per i builder
-
-        public Builder nome(String nome){
-            this.nome = nome;
-            return this;
-        }
-        public Builder spesaTotalePerAttivitaCommerciale(Map<AttivitaCommercialeModel, Double> spesaTotalePerAttivitaCommerciale){
-            this.spesaTotalePerAttivitaCommerciale = spesaTotalePerAttivitaCommerciale;
-            return this;
-        }
-        public Builder livelloPerAttivitaCommerciale(Map<ProgrammaALivelliModel, Integer> livelloPerAttivitaCommerciale){
-            this.livelloPerAttivitaCommerciale = livelloPerAttivitaCommerciale;
-            return this;
-        }
-        public Builder puntiPerAttivitaCommerciale(Map<ProgrammaAPuntiModel, Integer> puntiPerAttivitaCommerciale){
-            this.puntiPerAttivitaCommerciale = puntiPerAttivitaCommerciale;
-            return this;
-        }
-        public Builder saldoPerAttivitaCommerciale(Map<ProgrammaCashbackModel, Double> saldoPerAttivitaCommerciale){
-            this.saldoPerAttivitaCommerciale = saldoPerAttivitaCommerciale;
-            return this;
-        }
-
-        //Metodo build per il builder
-        public ClienteModel build(){
-            ClienteModel clienteModel = new ClienteModel();
-            clienteModel.nome = this.nome;
-            clienteModel.spesaTotalePerAttivitaCommerciale = this.spesaTotalePerAttivitaCommerciale;
-            clienteModel.livelloPerAttivitaCommerciale = this.livelloPerAttivitaCommerciale;
-            clienteModel.puntiPerAttivitaCommerciale = this.puntiPerAttivitaCommerciale;
-            clienteModel.saldoPerAttivitaCommerciale = this.saldoPerAttivitaCommerciale;
-            return clienteModel;
-        }
-
-
-    }
 
 }
