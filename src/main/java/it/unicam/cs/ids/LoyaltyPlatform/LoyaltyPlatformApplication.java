551a89200a8e9742ee0b3e80bf1065f72a44ee68
loris minetti <lorisminetti1997@gmail.com>
2023-06-01 17:42:03 +0200

    to check - trying do something easier

--- src/main/java/it/unicam/cs/ids/LoyaltyPlatform/LoyaltyPlatformApplication.java
+++ src/main/java/it/unicam/cs/ids/LoyaltyPlatform/LoyaltyPlatformApplication.java
@@ -4,35 +4,32 @@ import it.unicam.cs.ids.LoyaltyPlatform.controller.AttivitaCommercialeController
 import it.unicam.cs.ids.LoyaltyPlatform.controller.ClienteControllerImpl;
 import it.unicam.cs.ids.LoyaltyPlatform.controller.inbound.AttivitaCommercialeController;
 import it.unicam.cs.ids.LoyaltyPlatform.controller.inbound.ClienteController;
-import it.unicam.cs.ids.LoyaltyPlatform.model.Acquisto;
 import it.unicam.cs.ids.LoyaltyPlatform.model.AcquistoModel;
+import it.unicam.cs.ids.LoyaltyPlatform.model.AttivitaCommercialeModel;
 import it.unicam.cs.ids.LoyaltyPlatform.model.ClienteModel;
+import it.unicam.cs.ids.LoyaltyPlatform.model.ProgrammaCashbackModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-import java.util.Scanner;
+import java.util.*;
 
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }) // Disabilita la connessione al database
 public class LoyaltyPlatformApplication {
-
-
-
-
-
 
 	public static void main(String[] args) {
 
 
 		SpringApplication.run(LoyaltyPlatformApplication.class, args);
 
-		ClienteModel cliente = new ClienteModel.Builder().nome("Dilana Peci").build();
 
 
-//
-//
-		ClienteControllerImpl clienteController = new ClienteControllerImpl();
+
+
+		ClienteController clienteController = new ClienteControllerImpl();
+		AttivitaCommercialeController attivitaCommercialeController = new AttivitaCommercialeControllerImpl();
+
 
 		/**
 		 * Builder pattern. Tenere in considerazione per il futuro
@@ -42,54 +39,141 @@ public class LoyaltyPlatformApplication {
 //						.nome("Riccardo Cantabè")
 //						.build()
 //				);
+		ProgrammaCashbackModel programmaCashbackModel = new ProgrammaCashbackModel();
 
-		Scanner scanner = new Scanner(System.in);
-		startPlatform(scanner);
+
+
+		programmaCashbackModel.setNome("Cashback");
+		programmaCashbackModel.setSpesaMinima(0.0);
+		programmaCashbackModel.setPercentualeCashback(10);
+		programmaCashbackModel.setCosto(60);
+
+		Map<ProgrammaCashbackModel, Double> saldoPerAttivitaCommerciale = new HashMap<>();
+		saldoPerAttivitaCommerciale.put(programmaCashbackModel, 10.5);
+
+		ClienteModel cliente = ClienteModel.builder()
+				.nome("Loris Minetti")
+				.saldoPerAttivitaCommerciale(saldoPerAttivitaCommerciale)
+				.build();
+
+		AttivitaCommercialeModel attivitaCommercialeModel = AttivitaCommercialeModel.builder()
+				.nome("Nirvana da Safi")
+				.partitaIVA("")
+				.programmiFedeltaAderiti( List.of( programmaCashbackModel ))
+				.build();
+
+		programmaCashbackModel.setAttivitaCommerciale(attivitaCommercialeModel);
+
+		clienteController.effettuaAcquisto(cliente, attivitaCommercialeModel, 50);
+
+		System.out.println(cliente);
+
+
+
+
+
+
+
+//		Scanner scanner = new Scanner(System.in);
+//		startPlatform(scanner);
 
 	}
 
+
+
 	public static void startPlatform(Scanner scanner) {
+		AttivitaCommercialeControllerImpl attivitaCommercialeControllerImpl = new AttivitaCommercialeControllerImpl();
+
+
 		System.out.println("[INFO]: Avvio il sistema LoyaltyPlatform ...");
 		//TODO: capire se l'utente è un cliente o un'attività commerciale
 		System.out.println("[INFO]: Sei un cliente o un'attività commerciale? [C/A]");
 		String input = scanner.nextLine();
 		if (input.equalsIgnoreCase("C")) {
 			//TODO: cliente deve inserire il nome con input "Nome Cognome" affinchè non inserisce un nome corretto che venga trovato nel file system
+			ClienteModel clienteModel;  //tengo salvato il cliente
+			System.out.println("[INFO]: Inserisci il tuo nome: ");
+
 			while(true) {
-				System.out.println("[INFO]: Inserisci il tuo nome: ");
+
 				input = scanner.nextLine();
 				if (input.matches("[a-zA-Z]+ [a-zA-Z]+")) {
 					//TODO: controllare che il nome inserito sia presente nel file system
 					ClienteControllerImpl clienteControllerImpl = new ClienteControllerImpl();
-					ClienteModel cliente = clienteControllerImpl.getCliente(input);
+					try {
+						clienteModel = clienteControllerImpl.getByName(input);
+						break;
+					} catch (Exception e) {
+						e.getMessage();
+					}
+				} else System.out.println("Inserire il nome nel formato corretto 'Nome Cognome'");
+			}   //Caricamento cliente
 
-					//TODO: finire di implementare metodo che restituisce il cliente dal file system (insieme a tutti gli altri metodi getById) <-> attenzione per la classe CLiente nel quale il find viene inteso per Nome
+			System.out.println("[INFO]: Avvio il sistema per il cliente: "+ clienteModel.getNome());
 
-			System.out.println("[INFO]: Avvio il sistema come cliente ...");
+
+			//TODO: menu a tendina he chiede se il cliente vuole modificare i suoi dati, eliminarsi o effettuare un acquisto
+			System.out.println("[INFO]: Salve "+ clienteModel.getNome() +". Seleziona un azione: " +
+					"\n - 1. Effettua un acquisto " +
+					"\n - 2. Modifica dati utente " +
+					"\n - 3. Elimina utente");
+			input = scanner.nextLine();
+			switch(Integer.parseInt(input)) {
+				case 1 -> {
+					System.out.println("[INFO]: Inserisci l'id dell'attività commerciale: ");
+					input = scanner.nextLine();
+					AttivitaCommercialeModel attivitaCommercialeModel;
+					while(true) {  //id attività commmericiale deve essere corretto
+						try {
+							attivitaCommercialeModel = attivitaCommercialeControllerImpl.getById( UUID.fromString(input) );
+							break;
+						} catch (Exception e) {
+							e.getMessage();
+						}
+					}
+
+					System.out.println("[INFO]: Inserisci il valore dell'acquisto: ");
+						input = scanner.nextLine();
+						//TODO: controllare che il valore dell'acquisto sia un numero maggiore di 0
+						if (input.matches("[0-9]+")) {
+							System.out.println("[INFO]: Confermi l'acquisto? [Y/N]");
+							input = scanner.nextLine();
+							if (input.equalsIgnoreCase("Y")) {
+								System.out.println("[INFO]: Acquisto confermato!");
+								//TODO: la conferma dell'acquisto fa si che viene creato un nuovo acquisto nel file system, aggiornati i punti del cliente e aggiornato il livello del cliente
+								AcquistoModel acquisto = new AcquistoModel();
+								acquisto.setClienteModel(clienteModel);
+							//	acquistoControllerImpl.createAcquisto(acquisto);
+
+							} else if(input.equalsIgnoreCase("N")){
+								System.out.println("Acquisto annullato");
+							}
+						} else {
+							System.out.println("[ERROR]: Input non valido!");
+						}
+					}
+
+					//Per effettuare un acquisto viene mostrata la lista dei negozi
+					//la lista degli articoli
+					//viene chiesto se si vuole proseguire
+					//acquisto effettuato -> viene mostrato lo stato punti del cliente.
+				case 2 -> {}
+					//Viene chiesto quale campo dovrà essere modificato
+					//viene inserito e controllata la correttezza
+					//viene modificato -> vengono mostrati i dati del cliente
+				case 3 -> {}
+					//viene chiesto se si vuole davvero proseguire con l'eliminazione
+					//viene eliminato -> "cliente eliminato con successo"
+					//si torna al momento iniziale.
+			}
+
+
 			//TODO: cliente può effettuare un acquisto
+
 			System.out.println("[INFO]: Vuoi effettuare un acquisto? [Y/N]");
 			input = scanner.nextLine();
 			if (input.equalsIgnoreCase("Y")) {
-				System.out.println("[INFO]: Inserisci l'id dell'attività commerciale: ");
-				input = scanner.nextLine();
-				if (input.equalsIgnoreCase("1")) {
-					System.out.println("[INFO]: Inserisci il valore dell'acquisto: ");
-					input = scanner.nextLine();
-					//TODO: controllare che il valore dell'acquisto sia un numero maggiore di 0
-					if (input.matches("[0-9]+")) {
-						System.out.println("[INFO]: Confermi l'acquisto? [Y/N]");
-						input = scanner.nextLine();
-						if (input.equalsIgnoreCase("Y")) {
-							System.out.println("[INFO]: Acquisto confermato!");
-							//TODO: la conferma dell'acquisto fa si che viene creato un nuovo acquisto nel file system, aggiornati i punti del cliente e aggiornato il livello del cliente
-							AcquistoModel acquisto = new AcquistoModel();
-							acquisto.setClienteModel(cliente);
-							acquistoControllerImpl.createAcquisto(acquisto);
 
-						}
-					} else {
-						System.out.println("[ERROR]: Input non valido!");
-					}
 
 
 				} else if (input.equalsIgnoreCase("A")) {
@@ -98,10 +182,6 @@ public class LoyaltyPlatformApplication {
 					System.out.println("[ERROR]: Input non valido!");
 				}
 			}
-
-
 		}
 	}
 
-
-}
