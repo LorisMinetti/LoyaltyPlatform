02d11873865ab3a14008c5a2f98e7cb66e606bb6
loris minetti <lorisminetti1997@gmail.com>
2023-07-04 18:53:40 +0200

    RecordScadutiService

--- src/main/java/it/unicam/cs/ids/LoyaltyPlatform/RecordScadutiService.java
+++ src/main/java/it/unicam/cs/ids/LoyaltyPlatform/RecordScadutiService.java
@@ -1,7 +1,9 @@
 package it.unicam.cs.ids.LoyaltyPlatform;
 
 import it.unicam.cs.ids.LoyaltyPlatform.model.AdesioneProgrammaFedeltaModel;
+import it.unicam.cs.ids.LoyaltyPlatform.model.NotificaModel;
 import it.unicam.cs.ids.LoyaltyPlatform.repository.AdesioneProgrammaFedeltaRepository;
+import it.unicam.cs.ids.LoyaltyPlatform.repository.NotificaRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -16,12 +18,20 @@ public class RecordScadutiService {
 
     @Autowired
     private AdesioneProgrammaFedeltaRepository adesioneProgrammaFedeltaRepository;
+    @Autowired
+    private NotificaRepository notificaRepository;
 
-    @Scheduled(fixedRate = 12 * 60 * 60 * 1000) // Esegui ogni 24 ore
+    @Scheduled(fixedRate =  10 * 60 * 1000) // Esegui ogni 10 minuti
     public void eliminaRecordScaduti() {
         LocalDateTime now = LocalDateTime.now();
         List<AdesioneProgrammaFedeltaModel> adesioniScadute = adesioneProgrammaFedeltaRepository.findAllByRinnovoAutomaticoIsFalseAndDataScadenzaIsBefore(now);
         adesioneProgrammaFedeltaRepository.deleteAll(adesioniScadute);
     }
 
+    @Scheduled(fixedRate = 24 * 60 * 60 * 1000)
+    public void eliminaVecchieNotifiche(){
+        LocalDateTime now = LocalDateTime.now();
+        List<NotificaModel> notificheDeletable = notificaRepository.findAllByDestroyTimeBefore(now);
+        notificaRepository.deleteAll(notificheDeletable);
+    }
 }
